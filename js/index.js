f41c7ab7587d7db1a6ec1aefd3572915e7b669fa
Benjamin Congdon <bencongdon96@gmail.com>
2016-03-04 20:58:16 -0600

    Moving to Jekyll

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// window.addEventListener("scroll", function() {
+//     if (window.scrollY > 400) {
+//         $('.navbar').fadeIn();
+//     }
+//     else {
+//         $('.navbar').fadeOut();
+//     }
+// },false);
+
+// $(document).ready(function(){
+// 	if (window.scrollY < 400) {
+//         $('.navbar').hide();
+//     }
+// });
